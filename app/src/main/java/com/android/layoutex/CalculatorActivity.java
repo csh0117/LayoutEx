d006cdc8ed2e6081aae4aef346970d1e3c0451a3
csh0117 <ckdbrud8678@gmail.com>
2019-11-14 21:28:54 +0900

    2교시 끝

---
+++ app/src/main/java/com/android/layoutex/CalculatorActivity.java
@@ -0,0 +1,20 @@
+package com.android.layoutex;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CalculatorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.layout2);
+
+
+
+
+
+    }
+}
